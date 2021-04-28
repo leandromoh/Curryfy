96b9e2eb6c8c659c8327d6fa3206575ccbc49837
Leandro Fernandes <leandromoh@hotmail.com>
2021-04-28 20:40:05 -0300

    improve discard approach (#4)
    * add Discard
    
    * improve readme

--- Curry/Discard.cs
+++ Curry/Discard.cs
@@ -2,11 +2,20 @@
 {
     public class __
     {
-        public readonly static __ _;
 
-        static __()
+    }
+
+    public class Discard : __
+    {
+        public readonly static Discard _;
+        public readonly static Discard __;
+
+        private Discard() { }
+
+        static Discard()
         {
-            _ = new __();
+            _ = null;
+            __ = null;
         }
     }
 }
